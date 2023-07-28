f55f38661a566ad34422fc83366bb2e2021922de
Iamneo <techadmin@iamneo.ai>
2023-07-28 07:55:55 +0000

    Backup Commit

--- demo/src/test/java/utils/LoggerHandler.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class LoggerHandler {
-
-    public static final Logger log = LoggerFactory.getLogger(LoggerHandler.class);
-}
